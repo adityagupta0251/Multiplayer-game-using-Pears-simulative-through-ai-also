c7dd8d84c910c3ee6cb30f7e39a33c648bdbd948
adityagupta0251 <itsokkadi0251@gmail.com>
2025-03-25 20:33:06 +0530

    push also

---
+++ frontend/phaser-app/dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,7 @@
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'phaser-app',
+  location: 'us-central1'
+};
+
---
+++ frontend/phaser-app/dataconnect-generated/js/default-connector/index.cjs.js
@@ -0,0 +1,8 @@
+
+const connectorConfig = {
+  connector: 'default',
+  service: 'phaser-app',
+  location: 'us-central1'
+};
+exports.connectorConfig = connectorConfig;
+
